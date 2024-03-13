8883502fc27d8a3048339e7357f6e47a75713975
Jose R. Guignan <jrguignan@users.noreply.github.com>
2024-03-13 09:35:19 -0500

    origen

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+#importacion de librerias
+#import pandas as pd
+#import numpy as np
+
+#para el modelo
+#from sklearn.metrics.pairwise import cosine_similarity
+
+import streamlit as st
+
+st.write('Hola, Esto es un prueba!')
